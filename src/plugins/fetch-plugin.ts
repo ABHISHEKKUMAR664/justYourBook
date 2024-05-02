a0c295b185cb5e6113fd8bec9cd2ca03d957178d
Abhishek-it-dev <abhishek717151@gmail.com>
2024-05-02 17:36:23 +0530

    seperated  load filter

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -3,54 +3,80 @@ import axios from "axios";
 import localforage from "localforage";
 
 
-const fileCache= localforage.createInstance({
-    name:'fileCache',
+const fileCache = localforage.createInstance({
+  name: 'fileCache',
 });
 
 
-export const fetchPlugin=(inputCode:string)=>{
-    return {
-         name: 'fetch-plugin',
-         setup(build:esbuild.PluginBuild){
-            build.onLoad({ filter: /.*/ }, async (args:any) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents:inputCode,
-          };
-        }
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: 'fetch-plugin',
+    setup(build: esbuild.PluginBuild) {
 
-        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path); 
-        // if(cachedResult){
-        //     return cachedResult
-        // }
+      //1.
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+       });
 
-        const { data ,request} = await axios.get(args.path);
 
-        
-        const fileType=args.path.match(/.css$/) ? 'css':'jsx';
-        const contents=fileType ==='css'?
-        `
-        const style=document.createElement('style');
-        style.innerText= 'body{background-color:"red"}';
-        style.innerText= '';
-        document.head.appendChild(style);
-        `
-        :data;
+      //2.   
+      build.onLoad({filter:/.css$/},async(args:any)=>{
 
-        const result:esbuild.OnLoadResult ={
-          loader:'jsx',
-          contents,
-          resolveDir:new URL('./',request.responseURL).pathname,
-        }
-       
-        //store response in cache
-        await fileCache.setItem(args.path, result);
-
-         return result;
-          
-      });
-       
+         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path); 
+         if(cachedResult){
+             return cachedResult
          }
+
+         const { data, request } = await axios.get(args.path);
+         const escaped=data
+             .replace(/\n/g,'')
+             .replace(/"/g,'\\"')
+             .replace(/'/g,"\\'");
+
+         
+         const contents = 
+          `
+         const style=document.createElement('style');
+         style.innerText= 'body{background-color:"red"}';
+         style.innerText= '';
+         document.head.appendChild(style);
+         `
+
+         const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+         }
+          //store response in cache
+          await fileCache.setItem(args.path, result);
+          return result;
+         });
+
+
+       //3.
+       build.onLoad({ filter: /.*/ }, async (args: any) => {
+
+          const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path); 
+           if(cachedResult){
+              return cachedResult
+          }
+
+          const { data, request } = await axios.get(args.path);
+
+         const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents:data,
+          resolveDir: new URL('./', request.responseURL).pathname,
+         }
+         //store response in cache
+         await fileCache.setItem(args.path, result);
+          return result;
+         });
+
+       
+        }
     }
 }
