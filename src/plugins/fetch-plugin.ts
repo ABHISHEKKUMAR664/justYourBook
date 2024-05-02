a151f55b83f006cc1800d4ecd5938483ea2c41b9
Abhishek-it-dev <abhishek717151@gmail.com>
2024-05-02 16:40:05 +0530

    configure loading css file

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -1,16 +1,18 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
 import localforage from "localforage";
+
+
 const fileCache= localforage.createInstance({
     name:'fileCache',
 });
+
+
 export const fetchPlugin=(inputCode:string)=>{
     return {
          name: 'fetch-plugin',
          setup(build:esbuild.PluginBuild){
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
-
+            build.onLoad({ filter: /.*/ }, async (args:any) => {
         if (args.path === "index.js") {
           return {
             loader: "jsx",
@@ -18,23 +20,37 @@ export const fetchPlugin=(inputCode:string)=>{
           };
         }
 
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-        
-        if(cachedResult){
-            return cachedResult
-        }
+        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path); 
+        // if(cachedResult){
+        //     return cachedResult
+        // }
 
         const { data ,request} = await axios.get(args.path);
-        const result ={
-             loader: "jsx",
-          contents: data,
+
+        
+        const fileType=args.path.match(/.css$/) ? 'css':'jsx';
+        const contents=fileType ==='css'?
+        `
+        const style=document.createElement('style');
+        style.innerText= 'body{background-color:"red"}';
+        style.innerText= '';
+        document.head.appendChild(style);
+        `
+        :data;
+
+        const result:esbuild.OnLoadResult ={
+          loader:'jsx',
+          contents,
           resolveDir:new URL('./',request.responseURL).pathname,
         }
        
         //store response in cache
-        await fileCache.setItem(args.path, result)
-      });
+        await fileCache.setItem(args.path, result);
 
+         return result;
+          
+      });
+       
          }
     }
 }
